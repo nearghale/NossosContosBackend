b19e26e09f38d35dda25d3a03a44d0b19c6b5ef6
nearghale <nearghale@gmail.com>
2020-09-02 11:59:11 -0300

    Criação do primeiro commit

---
+++ Nossos Contos/Nossos Contos/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nossos_Contos.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        public string Index()
+        {
+            return "olá mundo!";
+        }
+    }
+}
