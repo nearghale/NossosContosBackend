e0341bd8fae12839708a68ff34f70444d0988217
Rafael Anacleto <rafael.anacleto12@gmail.com>
2020-09-04 17:08:19 -0300

    Criação da denúncia

---
+++ Nossos Contos/Nossos Contos/Controllers/ComplaintController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nossos_Contos.Model;
+using Nossos_Contos.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nossos_Contos.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+
+    public class ComplaintController : Controller
+    {
+
+        private Repositories.MongoDB.PersistentRepository<Entities.Complaint> complaintRepository;
+        private Repositories.MongoDB.PersistentRepository<Entities.Tale> taleRepository;
+        private ComplaintService complaintService;
+
+        public ComplaintController(DatabaseSettings databaseSettings)
+        {
+
+            complaintRepository = new Repositories.MongoDB.PersistentRepository<Entities.Complaint>(databaseSettings, "complaint");
+            taleRepository = new Repositories.MongoDB.PersistentRepository<Entities.Tale>(databaseSettings, "tale");
+            complaintService = new ComplaintService(taleRepository, complaintRepository);
+
+        }
+
+        [HttpPost]
+        public ActionResult<Entities.Complaint> Create(Entities.Complaint complaint)
+        {
+            var tale = taleRepository.FirstOrDefault(t => t.id == complaint.IDTale);
+            if (tale == null)
+                return this.Unauthorized("TALE_NOT_FOUNDED");
+
+            return complaintService.Create(complaint);
+            
+        }
+
+    }
+}
---
+++ Nossos Contos/Nossos Contos/Services/ComplaintService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nossos_Contos.Services
+{
+    public class ComplaintService
+    {
+        private Repositories.MongoDB.PersistentRepository<Entities.Complaint> _complaintRepository;
+        private Repositories.MongoDB.PersistentRepository<Entities.Tale> _taleRepository;
+
+        public ComplaintService(Repositories.MongoDB.PersistentRepository<Entities.Tale> taleRepository, Repositories.MongoDB.PersistentRepository<Entities.Complaint> complaintRepository)
+        {
+            
+            _complaintRepository = complaintRepository;
+            _taleRepository = taleRepository;
+
+        }
+
+        public Entities.Complaint Create(Entities.Complaint complaint)
+        {
+            var newComplaint = new Entities.Complaint();
+
+            newComplaint.Denunciation = complaint.Denunciation;
+            newComplaint.id = complaint.id;
+            newComplaint.IDTale = complaint.IDTale;
+            newComplaint.TypeComplaint = complaint.TypeComplaint;
+            newComplaint.TitleComplaint = complaint.TitleComplaint;
+
+            return _complaintRepository.Create(newComplaint);
+        }
+    }
+}
